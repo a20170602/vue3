130c16c5ea4a109c08c6b0a5d657a15f602bc2de
cjylxl <cjylxl0602@126.com>
2021-10-24 23:07:42 +0800

    初始化vue3项目

---
+++ vue3_0/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    {{count}}
+  </div>
+</template>
+
+<script lang="ts">
+/**
+ * Composition API 用户层面
+ * vue 框架
+ * hook -> 组合起来 -> Vue3.0 Composition API 框架设计模式
+ * */
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'Home',
+  setup () {
+    console.log('setup')
+    const count = ref(0)
+
+    console.log('count==>', count)
+
+    return {
+      count
+    }
+  }
+})
+</script>
